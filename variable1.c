f91cb3fefb1174838968b4ff249b303806d23378
SSI Digital <42675358+ssiedu@users.noreply.github.com>
2022-05-03 19:07:53 +0530

    C Variable Programs

---
+++ variable1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int If=10;  //variable initialization
+	int b;		//variable declaration
+	b=20;		//variable assignment
+	
+	printf("\n value of a : %d",If);
+	printf("\n value of b : %d",b);
+	getch();
+}
---
+++ variable2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int a=20;  // global variable
+void main()
+{
+	int a;    //local variable
+	//printf("\n enter the value of a : ");
+	//scanf("%d",&a);
+	printf("\n value of a is : %d",a);
+	getch();
+}
